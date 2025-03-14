268b6f288dc9fa55f8985719fd2c9e52c25c73fa
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-03-15 01:37:12 +0700

    Update run.bat

--- aPriori/run.bat
+++ aPriori/run.bat
@@ -6,7 +6,7 @@ cd %~dp0
 
 echo Checking configuration files...
 
-(for %%F in (datas.txt wallets.txt proxies.txt) do (
+(for %%F in (privateKeys.txt proxies.txt) do (
     if not exist %%F (
         type nul > %%F
         echo Created %%F
