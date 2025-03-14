4794fae9a09c6bc9e99bd6ddf54a792559612248
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-03-15 01:37:29 +0700

    Update run.sh

--- aPriori/run.sh
+++ aPriori/run.sh
@@ -63,7 +63,7 @@ fi
 
 check_configs
 
-for file in datas.txt wallets.txt proxies.txt; do
+for file in privateKeys.txt proxies.txt; do
     if [ ! -f "$file" ]; then
         touch "$file"
         print_green "Created $file"
